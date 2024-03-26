b6b7e4f10d87309e9b9874dd575578133c4bc37f
Shobindiran <shobindiran@gmail.com>
2024-03-26 19:39:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+]; 
 
-function App() {
+export default  function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
+
   );
 }
 
-export default App;
+function Logo(){
+  return <h1>🏝️ FAR AWAY 💼</h1>
+};
+
+function Form(){
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip ❤️</h3>
+    </div>
+  )
+}
+
+function PackingList(){
+  return(
+    <div className="list">
+        <ul >
+          {initialItems.map(
+            (item)=> <Item item={item}/>
+            )
+          }
+        </ul>
+    </div>
+  )
+}
+
+function Item({item}){
+  return( 
+  <li key={item.id} style={item.packed?{textDecoration:"line-through"}: {} } >
+    <span>{item.quantity}X {item.description}</span>
+    <button>❌</button>
+  </li>
+  )
+}
+
+function Stats(){
+  return(
+    <footer className="stats">
+      <em>💼 You have X items on your list and you already packed X (X%) </em>
+    </footer>
+  )
+}
+
